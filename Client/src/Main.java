86dac05510c6e03639f2ceb34792b50cc86a8e45
thangnvit <thang6766@gmail.com>
2017-01-05 21:58:59 +0700

    ahi

--- Client/src/Main.java
+++ Client/src/Main.java
@@ -2,13 +2,19 @@
 
 //****************************************************************************************
 
+import org.thangnv.messenger_gui.Utils;
+
+import javax.swing.*;
 import java.awt.*;
-import java.net.UnknownHostException;
 
-public class Main {
+public class Main extends JFrame{
 
-    public Main() throws HeadlessException, UnknownHostException {
-
+    public Main() throws HeadlessException {
+        JLabel a = new JLabel();
+        a.setIcon(Utils.load("C:\\Users\\DEV\\Pictures\\Camera Roll\\avtar.jpg",50,50));
+        add(a);
+        setVisible(true);
+        setBounds(100,100,550,555);
     }
 
     public static void main(final String[] args) throws Exception {
